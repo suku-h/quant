8e478c34a22edaa60776cf7d35e5d38d7cfd1600
Sukumar <sukumar.honkote@gmail.com>
2017-11-17 06:53:26 +0530

    updated util

--- indian_stock_analysis/models/util.py
+++ indian_stock_analysis/models/util.py
@@ -1,5 +1,26 @@
+from copy import deepcopy
 import numpy as np
 
 
 def removeNaN(arr):
     return arr[~np.isnan(arr)]
+
+
+def getMaxDataPoints(res: np.ndarray, indicatorVals: np.ndarray, r1, r2, r3, r4):
+    if len(r1) == 0:
+        r1 = deepcopy(res)
+    if len(r2) == 0:
+        r2 = deepcopy(res)
+    if len(r3) == 0:
+        r3 = deepcopy(res)
+    if len(r4) == 0:
+        r4 = deepcopy(res)
+
+    return min(len(res), len(indicatorVals), len(r1), len(r2), len(r3), len(r4))
+
+
+def getUsableDataPoints(arr, max_len):
+    if len(arr) > 0:
+        return arr[len(arr) - max_len:]
+    else:
+        return arr
